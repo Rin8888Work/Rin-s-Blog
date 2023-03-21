fd731afc2e697af66b29c3d3aac004d4abb3bd2e
muoingon <phuocnv106@gmail.com>
2023-03-21 13:44:46 +0700

    chore: add gg analytic

--- data/siteMetadata.ts
+++ data/siteMetadata.ts
@@ -130,12 +130,12 @@ export let siteMetadata = {
 	youtube: 'https://www.youtube.com/@rin8888work',
 	linkedin: 'https://www.linkedin.com/in/rin8888work/',
 	locale: 'en-US',
-	analyticsURL: "https://analytics.umami.is/share/eWbwkSytsH6k4YrQ/Rin's%20Blog",
+	analyticsURL: 'https://analytics.umami.is/websites/af8b54df-c1fa-4a45-aded-32b6e8ecc1fd',
 	analytics: {
 		plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
 		simpleAnalytics: false, // true or false
 		umamiWebsiteId: 'af8b54df-c1fa-4a45-aded-32b6e8ecc1fd', // e.g. 123e4567-e89b-12d3-a456-426614174000
-		googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+		googleAnalyticsId: process.env.GG_ANALYTIC_ID || '', // e.g. UA-000000-2 or G-XXXXXXX
 	},
 	socialAccounts: {
 		github: 'rin8888work',
