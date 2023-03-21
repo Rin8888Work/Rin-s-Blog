b7d89ddf24850da62f2f7c3580ac54ec293648b2
muoingon <phuocnv106@gmail.com>
2023-03-21 15:36:14 +0700

    chore: config dmca, domain verify

--- data/siteMetadata.ts
+++ data/siteMetadata.ts
@@ -108,6 +108,8 @@ export const seedingLink = {
 };
 
 export let siteMetadata = {
+	domainVerifyId: '6oP9j3owBNBZc_m8OirYVDJM4UxVEN4fx-jhUVzQcMA',
+	dmcaVerifyId: 'fde0a6d4-3c92-4914-bccb-f1c7ded156b8',
 	introduceWebsite: 'introduceWebsite',
 	phoneNumber: 'phoneNumber',
 	title: 'title',
